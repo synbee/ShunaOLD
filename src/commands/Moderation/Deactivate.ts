8f8f989b21897b448ed0547d4ad2094f3933f944
synbee <97864273+synbee@users.noreply.github.com>
2022-03-04 10:05:27 +0530

    Update Deactivate.ts

--- src/commands/Moderation/Deactivate.ts
+++ src/commands/Moderation/Deactivate.ts
@@ -23,14 +23,14 @@ export default class Command extends BaseCommand {
     const type = joined.trim().toLowerCase() as toggleableGroupActions;
     if (!Object.values(toggleableGroupActions).includes(type))
       return void M.reply(
-        `🟥 Invalid Option: *${this.client.util.capitalize(type)}*`
+        `🟥 𝐈𝐧𝐯𝐚𝐥𝐢𝐝 𝐎𝐩𝐭𝐢𝐨𝐧: *${this.client.util.capitalize(type)}*`
       );
     const data = await this.client.getGroupData(M.from);
     if (!data[type])
       return void M.reply(
         `🟨 *${this.client.util.capitalize(
           type
-        )}* is already *inactived*, Baka!`
+        )}* 𝐢𝐬 𝐚𝐥𝐫𝐞𝐚𝐝𝐲 *𝐢𝐧𝐚𝐜𝐭𝐢𝐯𝐞𝐝*, !`
       );
     if (type === "news") {
       await this.client.DB.group.updateOne(
@@ -42,7 +42,7 @@ export default class Command extends BaseCommand {
         { $pull: { jids: M.from } }
       );
       return void M.reply(
-        `🟩 *${this.client.util.capitalize(type)}* is now inactive`
+        `🟩 *${this.client.util.capitalize(type)}* 𝐢𝐬 𝐧𝐨𝐰 𝐢𝐧𝐚𝐜𝐭𝐢𝐯𝐞`
       );
     }
     await this.client.DB.group.updateOne(
@@ -50,7 +50,7 @@ export default class Command extends BaseCommand {
       { $set: { [type]: false } }
     );
     return void M.reply(
-      `🟩 *${this.client.util.capitalize(type)}* is now inactive`
+      `🟩 *${this.client.util.capitalize(type)}* 𝐢𝐬 𝐧𝐨𝐰 𝐢𝐧𝐚𝐜𝐭𝐢𝐯𝐞`
     );
   };
 }
