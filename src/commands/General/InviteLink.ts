74c3d1e261160b78265dc41afb673f92130e1dbe
synbee <97864273+synbee@users.noreply.github.com>
2022-03-04 09:25:33 +0530

    Update InviteLink.ts

--- src/commands/General/InviteLink.ts
+++ src/commands/General/InviteLink.ts
@@ -19,17 +19,17 @@ export default class Command extends BaseCommand {
     run = async (M: ISimplifiedMessage): Promise<void> => {
         // check if Bot is the admin
         if (!M.groupMetadata?.admins?.includes(this.client.user.jid))
-            return void M.reply(`I'm not an admin of this group.`)
+            return void M.reply(`𝐼'𝑚 𝑛𝑜𝑡 𝑎𝑛 𝑎𝑑𝑚𝑖𝑛 𝑜𝑓 𝑡ℎ𝑖𝑠 𝑔𝑟𝑜𝑢𝑝.`)
         if ((await this.client.getGroupData(M.from)).invitelink) {
             const code = await this.client.groupInviteCode(M.from).catch(() => {
-                return void M.reply('Could not get the invite link')
+                return void M.reply('𝐶𝑜𝑢𝑙𝑑 𝑛𝑜𝑡 𝑔𝑒𝑡 𝑡ℎ𝑒 𝑖𝑛𝑣𝑖𝑡𝑒 𝑙𝑖𝑛𝑘')
             })
             await this.client.sendMessage(
                 M.sender.jid,
                 `*Invite link:* https://chat.whatsapp.com/${code}`,
                 MessageType.text
             )
-            return void M.reply('Sent you the Group Link in personal message')
+            return void M.reply('𝑺𝒆𝒏𝒕 𝒚𝒐𝒖 𝒕𝒉𝒆 𝑮𝒓𝒐𝒖𝒑 𝑳𝒊𝒏𝒌 𝒊𝒏 𝒑𝒆𝒓𝒔𝒐𝒏𝒂𝒍 𝒎𝒆𝒔𝒔𝒂𝒈𝒆')
         } else {
             return void M.reply(
                 `Command not enabled by the admin.\nUse *${this.client.config.prefix}act invitelink* to enable it`
