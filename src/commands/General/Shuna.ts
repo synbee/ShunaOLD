4804b14faed26eb884756d3995296e7c93b92658
synbee <97864273+synbee@users.noreply.github.com>
2022-03-06 13:05:37 +0530

    Update Shuna.ts

--- src/commands/General/Shuna.ts
+++ src/commands/General/Shuna.ts
@@ -9,16 +9,16 @@ import { ISimplifiedMessage } from "../../typings";
 export default class Command extends BaseCommand {
 	constructor(client: WAClient, handler: MessageHandler) {
 		super(client, handler, {
-			command: "chitoge",
+			command: "shuna",
 			description: "Displays the info",
 			category: "general",
-			usage: `${client.config.prefix}chitoge`,
+			usage: `${client.config.prefix}shuna`,
 			baseXp: 200,
 		});
 	}
 
 	run = async (M: ISimplifiedMessage): Promise<void> => {
-		const chitoge =
+		const shuna =
 			"https://user-images.githubusercontent.com/97864273/156913350-6426b182-c65b-4971-b964-c8d1849f2c41.mp4";
 		return void this.client.sendMessage(
 			M.from,
